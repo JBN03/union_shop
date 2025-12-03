5b94fed9466f9ef34f110fb8a32a61e8d45df960
JBN03 <julian.13bn@gmail.com>
2025-12-03 21:49:02 +0000

    Refactor: split home screen into hero and products grid

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,4 @@
-import 'dart:math' as math;
 import 'package:flutter/material.dart';
-import 'package:union_shop/widgets/product_card.dart';
-import 'package:union_shop/services/product_service.dart';
-import 'package:union_shop/models/product.dart' as model;
 import 'package:union_shop/widgets/header.dart';
 import 'package:union_shop/widgets/footer.dart';
 import 'package:union_shop/widgets/home_hero.dart';

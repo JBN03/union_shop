e2df931c19df32c122a214a725949a16a053664e
JBN03 <julian.13bn@gmail.com>
2025-12-04 19:17:11 +0000

    chore: Add PersonalisationPanel stub

---
+++ lib/widgets/personalisation_panel.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+/// Deprecated: personalisation UI moved into `PrintShackContent`.
+/// This stub remains for backwards compatibility but renders nothing.
+class PersonalisationPanel extends StatelessWidget {
+  const PersonalisationPanel({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox.shrink();
+  }
+}
